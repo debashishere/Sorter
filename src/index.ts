0163b677d6b355826acb6ff4530405a06f2cd281
debashishere <debashishere007@gmail.com>
2021-08-12 12:57:10 +0530

    adding string sorting feat

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Sorter } from "./Sorter";
+
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 50])
-const sorter = new Sorter(numbersCollection)
-sorter.sort();
-
+const numSorter = new Sorter(numbersCollection)
+numSorter.sort();
 console.log(numbersCollection.data)
+
+const charactersCollection = new CharactersCollection('XYaayb');
+const stringSorter = new Sorter(charactersCollection)
+stringSorter.sort()
+console.log(charactersCollection.data)
