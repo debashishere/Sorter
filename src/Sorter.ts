b7cdcecc33304cfa737440363189424faa29c445
debashishere <debashishere007@gmail.com>
2021-08-12 17:57:17 +0530

    removing Sorter Interface

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,10 +1,4 @@
- 
- 
- export interface Sortable {
-  length: number;
-  compare(leftIndex: number, rightIndex: number): boolean;
-  swap(leftIndex: number, rightIndex: number): void;
-}
+
 
 export abstract class Sorter {
   abstract compare(leftIndex: number, rightIndex: number): boolean;
