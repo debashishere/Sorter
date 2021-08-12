920a7974fb56d5e73be69a9786225c502f6f6cfd
debashishere <debashishere007@gmail.com>
2021-08-12 17:10:45 +0530

    Adding link list

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Sorter } from "./Sorter";
 
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 50])
 const numSorter = new Sorter(numbersCollection)
@@ -12,3 +13,13 @@ const charactersCollection = new CharactersCollection('XYaayb');
 const stringSorter = new Sorter(charactersCollection)
 stringSorter.sort()
 console.log(charactersCollection.data)
+
+const linklist = new LinkedList();
+linklist.add(5);
+linklist.add(7);
+linklist.add(1);
+linklist.add(0);
+linklist.add(9);
+const listSorter = new Sorter(linklist);
+listSorter.sort();
+linklist.print();
